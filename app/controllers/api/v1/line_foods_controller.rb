a060d954c0b312f43f099fc5edf861d518428be8
taki-21 <taki.it.0208.21@gmail.com>
2021-09-21 21:19:35 +0900

    Add: line_foods_controller

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -1,47 +1,77 @@
-class Api::V1::LineFoodController < ApplicationController
-  before_action :set_food, only: %i[create]
+module Api
+  module V1
+    class LineFoodsController < ApplicationController
+      before_action :set_food, only: %i[create replace]
+      def index
+        line_foods = LineFood.active
+        if line_foods.exists?
+          render json: {
+            line_food_ids: line_foods.map { |line_food| line_food.id },
+            restaurant: line_foods[0].restaurant,
+            count: line_foods.sum { |line_food| line_food[:count] },
+            amount: line_foods.sum { |line_food| line_food.total_amount },
+          }, status: :ok
+        else
+          render json: {}, status: :no_content
+        end
+      end
 
-  def create
-    if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exist?
-      return render json: {
-        existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
-        new_restaurant: Food.find(params[:food_id]).restaurant.name
-      }, status: :not_acceptable
-    end
+      def create
+        if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
+          return render json: {
+            existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
+            new_restaurant: Food.find(params[:food_id]).restaurant.name,
+          }, status: :not_acceptable
+        end
 
-    set_line_food(@ordered_foods)
+        set_line_food(@ordered_food)
 
-    if @line_food.save
-      render json: {
-        line_food: @line_food
-      }, status: :created
-    else
-      render json: {}, status: :internal_server_error
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      def replace
+        LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
+          line_food.update_attribute(:active, false)
+        end
+
+        set_line_food(@ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      private
+
+      def set_food
+        @ordered_food = Food.find(params[:food_id])
+      end
+
+      def set_line_food(ordered_food)
+        if ordered_food.line_food.present?
+          @line_food = ordered_food.line_food
+          @line_food.attributes = {
+            count: ordered_food.line_food.count + params[:count],
+            active: true
+          }
+        else
+          @line_food = ordered_food.build_line_food(
+            count: params[:count],
+            restaurant: ordered_food.restaurant,
+            active: true
+          )
+        end
+      end
     end
   end
-
-  private
-
-  def set_food
-    @ordered_foods = Food.find(params[:food_id])
-  end
-
-  # 正常な仮注文
-  def set_line_food(ordered_food)
-    if ordered_food.line_food.present?
-      @line_food = ordered_food.line_food
-      @line_food.attributes = {
-        count: ordered_food.line_food.count + params[:count],
-        active: true,
-      }
-    else
-      @line_food = ordered_food.build_line_food(
-        count: params[:count],
-        restaurant: ordered_food.restaurant,
-        active: true,
-      )
-    end
-
-  end
-
 end
